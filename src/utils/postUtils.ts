9004db3d7487c872f73c11f03cf7a5e1dbb55198
BencePasztor <pasztorbencee@gmai.com>
2024-02-06 13:57:18 +0100

    refactor: extend post image download with additional checks

--- src/utils/postUtils.ts
+++ src/utils/postUtils.ts
@@ -2,20 +2,32 @@ import sharp from 'sharp';
 import path from 'path';
 
 export const savePostImage = async (imageUrl: string) => {
+    //Check the validity of the imageUrl
+    if (!imageUrl) {
+        return '/images/post/noimage.webp'
+    }
+
     try {
+        //Fetch Image
         const imageResponse = await fetch(imageUrl)
+
+        //Check Content type (only images are allowed)
+        const contentType = imageResponse.headers.get('content-type')
+        if (!contentType || !contentType.startsWith('image/')) {
+            throw new Error('Invalid Content Type')
+        }
+
+        //Generate file name
         const generatedFileName = crypto.randomUUID() + '.webp'
 
+        //Resize, convert and save image
         await sharp(await imageResponse.arrayBuffer())
             .resize(300, 300)
             .toFile(path.resolve(__dirname, '..', 'public', 'images', 'posts', generatedFileName))
 
+        //Return path to image
         return `/images/posts/${generatedFileName}`
     } catch (error) {
-        console.log(error)
         return '/images/post/noimage.webp'
     }
 }
-
-// TODO
-export const deletePostImage = () => { }
