1b1695616160082e5f87d65bf375225b954df8a8
BencePasztor <pasztorbencee@gmai.com>
2024-02-07 15:05:20 +0100

    feat: add character limit to post sourceUrl

--- src/schemas/postSchema.ts
+++ src/schemas/postSchema.ts
@@ -2,7 +2,7 @@ import { z } from "zod"
 import zodValidatorFactory from "@/utils/zodValidatorFactory"
 
 export const createPostSchema = z.object({
-    sourceUrl: z.string().trim().url("Invalid URL").transform(urlString => {
+    sourceUrl: z.string().trim().max(2083, 'The max lenght of the URL is 2083 characters').url("Invalid URL").transform(urlString => {
         const cleanUrl = new URL(urlString);
         cleanUrl.hash = '';
         cleanUrl.search = '';
