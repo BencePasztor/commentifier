df38a6c27bf54b31956758a26f097227a28fd919
BencePasztor <pasztorbencee@gmai.com>
2024-03-14 22:26:08 +0100

    feat: add inter font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif']
+      },
       colors: {
         primary: {
           DEFAULT: '#63A94A',
